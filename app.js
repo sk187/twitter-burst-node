d508499e57871bc3e8c2c39843f17b5e7afa0350
Emily C. McAfee <emilymcafee1@gmail.com>
2015-01-28 15:48:06 -0500

    allow credentials true

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'https://twitterburst.herokuapp.com');
   res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Credentials', 'true');
   return next();
 });
 
